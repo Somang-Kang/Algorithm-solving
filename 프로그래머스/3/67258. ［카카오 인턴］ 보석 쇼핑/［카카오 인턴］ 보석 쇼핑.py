f81838b80ede4f20043978c2d7cfcf634336768c
Somangmang <77764316+Somang-Kang@users.noreply.github.com>
2024-03-05 23:52:07 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 95.81 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,25 +1,28 @@
 def solution(gems):
-    N = len(gems)
-    answer = [0, N-1]
-    kind = len(set(gems))  # 보석종류
-    dic = {gems[0]:1,}  # 종류 체크할 딕셔너리
-    s,e = 0,0  # 투포인터
-    while s<N and e<N:
-        if len(dic) < kind:  # 종류 부족하면 end point 증가 & dic 개수 증가
-            e += 1
-            if e == N:
+    answer = [0, len(gems)-1]
+    
+    start_idx = 0
+    end_idx = 0
+    gems_list = set(gems)
+    gems_dict = {gems[0]:1}
+    
+    while start_idx<len(gems) and end_idx<len(gems):
+        if len(gems_dict) < len(gems_list):
+            end_idx +=1
+            if end_idx == len(gems):
                 break
-            dic[gems[e]] = dic.get(gems[e], 0) + 1
-            
-        else:  # 종류 같으면 ans 비교 후 답 갱신하고, start point 증가 & dic 개수 다운
-            if (e-s+1) < (answer[1]-answer[0]+1):
-                answer = [s,e]
-            if dic[gems[s]] == 1:
-                del dic[gems[s]]
+            if gems[end_idx] in gems_dict:
+                gems_dict[gems[end_idx]] += 1
             else:
-                dic[gems[s]] -= 1
-            s += 1
-
+                gems_dict[gems[end_idx]] = 1
+        else:
+            if (end_idx-start_idx+1) < (answer[1]-answer[0]+1):
+                answer = [start_idx,end_idx]
+            if gems_dict[gems[start_idx]] == 1:
+                del gems_dict[gems[start_idx]]
+            else:
+                gems_dict[gems[start_idx]] -= 1
+            start_idx += 1
     answer[0] += 1
     answer[1] += 1
     return answer
